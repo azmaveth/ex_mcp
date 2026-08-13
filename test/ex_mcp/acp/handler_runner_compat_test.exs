003c9ddb2cdb19eaac0a94ac116411328a2f7cd6
azmaveth <hysun@hysun.com>
2026-08-13 02:05:53 +0000

    style: format HandlerRunner compat test

--- test/ex_mcp/acp/handler_runner_compat_test.exs
+++ test/ex_mcp/acp/handler_runner_compat_test.exs
@@ -23,7 +23,8 @@ defmodule ExMCP.ACP.Client.HandlerRunnerCompatTest do
     def handle_file_read(_session_id, _path, _opts, state), do: {:error, :not_supported, state}
 
     @impl true
-    def handle_file_write(_session_id, _path, _content, state), do: {:error, :not_supported, state}
+    def handle_file_write(_session_id, _path, _content, state),
+      do: {:error, :not_supported, state}
 
     @impl true
     def handle_terminal_request(_method, _params, _id, state), do: {:error, :not_supported, state}
@@ -35,7 +36,9 @@ defmodule ExMCP.ACP.Client.HandlerRunnerCompatTest do
   test "session_update/3 remains as an rc.6 compatibility wrapper" do
     {:ok, pid} = HandlerRunner.start_link(CaptureHandler, [pid: self()], self())
 
-    assert :ok = HandlerRunner.session_update(pid, "s1", %{"sessionUpdate" => "agent_message_chunk"})
+    assert :ok =
+             HandlerRunner.session_update(pid, "s1", %{"sessionUpdate" => "agent_message_chunk"})
+
     assert_receive {:session_update, "s1", %{"sessionUpdate" => "agent_message_chunk"}}
   end
 end
